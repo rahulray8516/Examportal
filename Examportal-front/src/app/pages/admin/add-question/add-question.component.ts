117fae080c40c03d8063216caebe9210d1f29fb3
RAHUL RAY <ray.rahul.8101@gmail.com>
2023-02-27 19:13:50 +0530

    My examportal frontend

---
+++ Examportal-front/src/app/pages/admin/add-question/add-question.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuestionService } from 'src/app/services/question.service';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
+ 
+@Component({
+  selector: 'app-add-question',
+  templateUrl: './add-question.component.html',
+  styleUrls: ['./add-question.component.css'],
+})
+export class AddQuestionComponent implements OnInit {
+   qId;
+  qTitle;
+ question = {
+    quiz : {},
+    content: 'b',
+    option1: 'n',
+    option2: 'n',
+    option3: '',
+    option4: '',
+    answer: 'n',
+  };
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _question: QuestionService
+  ) {}
+
+  ngOnInit(): void {
+    this.qId = this._route.snapshot.params.qid;
+    this.qTitle = this._route.snapshot.params.title;
+    this.question.quiz['qId'] = this.qId;
+  }
+
+  formSubmit() {
+    if (this.question.content.trim() == '' || this.question.content == null) {
+      return;
+    }
+
+    if (this.question.option1.trim() == '' || this.question.option1 == null) {
+      return;
+    }
+    if (this.question.option2.trim() == '' || this.question.option2 == null) {
+      return;
+    }
+    if (this.question.answer.trim() == '' || this.question.answer == null) {
+      return;
+    }
+
+    //form submit
+    this._question.addQuestion(this.question).subscribe(
+      (data: any) => {
+        Swal.fire('Success ', 'Question Added. Add Another one', 'success');
+        this.question.content = '';
+        this.question.option1 = '';
+        this.question.option2 = '';
+        this.question.option3 = '';
+        this.question.option4 = '';
+        this.question.answer = '';
+      },
+      (error) => {
+        Swal.fire('Error', 'Error in adding question', 'error');
+      }
+    );
+  }
+}
